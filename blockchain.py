acd4986cdab75fa00a379c8a65633cac0709c304
Lucas Cardoso <lucas.gavirachi@edu.pucrs.br>
2022-06-14 15:37:57 -0300

    Updating the blockchain. Creating flask app

--- blockchain.py
+++ blockchain.py
@@ -16,6 +16,7 @@ class Blockchain:
                  'proof': proof,
                  'previous_hash': previous_hash}
         self.chain.append(block)
+        return block
 
     def get_previous_block(self):
         """Gets previous block."""
@@ -37,3 +38,54 @@ class Blockchain:
         """Encodes the block to a sha256 hash."""
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        """Checks if the chain is valid. """
+        previous_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            block = chain[block_index]
+
+            # Verify the previous hashes
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            
+            # Verify if the block hash is valid, as well as the proof of work.
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(
+                str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            
+            previous_block = block
+            block_index += 1
+
+        return True
+
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    # Gather the previous block info.
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    previous_hash = blockchain.hash(previous_block)
+    # Call the method to mine (search for the correct nounce).
+    proof = blockchain.proof_of_work(previous_proof)
+    # Creates a new block in the blockchain after finding the nounce.
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {'message': 'You just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+
+    # Returns the response with some info and the HTTP Code 200.
+    return jsonify(response), 200
+
